8ec82b8aa0884cd5acb94ed1e4bedc7d40bea713
Khoa51600041 <44065823+Khoa51600041@users.noreply.github.com>
2018-12-03 14:37:12 +0700

    replace
    Co-Authored-By: nguyenndong <nguyenndong@users.noreply.github.com>

--- fetch.php
+++ fetch.php
@@ -1,22 +1,20 @@
 <?php
   //fetch.php
+  header('Content-Type: text/html; charset=UTF-8');
   $connect = mysqli_connect("localhost", "root", "", "quanlydoan");
   $output = '';
+  $output1 = '';
+  $output2 = array();
   if(isset($_POST["query"])) {
   $search = mysqli_real_escape_string($connect, $_POST["query"]);
   $query = "
     SELECT * FROM sinh_vien 
     WHERE maSV LIKE '%".$search."%'";
   }
-  else {
-    $query = "
-    SELECT * FROM sinh_vien
-    ";
-  }
 
   $result = mysqli_query($connect, $query);
   if(mysqli_num_rows($result) > 0) {
-    $output .= '
+    $output2['table'] = '
     <div class="table-responsive">
       <table class="table" id="table-modal" style="margin-left: 0px;">
         <tr>
@@ -26,15 +24,11 @@
         </tr>
     ';
     $row = mysqli_fetch_array($result);
-    $output .= '
-    <tr>
-      <td>'.$row["maSV"].'</td>
-      <td>'.$row["tenSV"].'</td>
-      <td>'.$row["maLop"].'</td>
-      <td><button type="submit" class="btn"><i class="fa fa-check"></i></button></td>
-    </tr>';
-    echo $output;
-  } else {
-    echo 'Data Not Found';
+    $output2['row'] = $row;
+    //echo $output1;
+    //echo $output;
+
+    echo json_encode($output2);
+    $connect->close();
   }
 ?>
